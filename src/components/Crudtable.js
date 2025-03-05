cd62908abd5747398a25468398fcd12fd392587b
Deksha <dekshachaudhary91@gmail.com>
2025-03-05 10:25:01 +0530

    change

--- src/components/Crudtable.js
+++ src/components/Crudtable.js
@@ -1,47 +1,49 @@
 import React, { useState, useEffect } from 'react'
 
 const Crudtable = () => {
+    const[formInfo,setFormInfo]=useState({name:'',email:'',number:''})
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [number, setNumber] = useState('')
     const [data, setData] = useState([])
     const [editIndex, setEditIndex] = useState(null);
+    console.log(formInfo);
+    
 
     useEffect(() => {
-        const savedData = localStorage.getItem('data');
-        if (savedData) {
-            setData(JSON.parse(savedData));
-        }
+        fetchData()
     }, []);
 
     
-    useEffect(() => {
-        if (data.length > 0) {
-            localStorage.setItem('data', JSON.stringify(data));
-        }
-    }, [data]);
+    
 
+const fetchData=()=>{
+    let formdata=sessionStorage.getItem("formdata")
+    setData(JSON.parse(formdata))
+}
 
     const saveData = (e) => {
-       
-        if (name && email && number) {
-          if (editIndex !== null) {
-           
-            const updatedData = [...data];
-            updatedData[editIndex] = { name, email, number };
-            setData(updatedData);
-            setEditIndex(null); 
-          } else {
-           
-            setData([...data, { name, email, number }]);
-          }
+       if(formInfo.name==undefined ||formInfo.name==null ||formInfo.name==''){
+        alert("name can't be blank")
+        return ;
+       }
+
+       if(formInfo.email==undefined ||formInfo.email==null ||formInfo.email==''){
+        alert("email can't be blank")
+        return ;
+       }
+       if(formInfo.number==undefined ||formInfo.number==null ||formInfo.number==''){
+        alert("number can't be blank")
+        return ;
+       }
+         let alldata=[formInfo]
          
-          setName('');
-          setEmail('');
-          setNumber('');
-        } else {
-          alert('Please fill all fields!');
-        }
+if(data?.length >0){
+alldata=[...data,formInfo]
+}
+         sessionStorage.setItem('formdata',JSON.stringify(alldata))
+         fetchData()
+         setFormInfo({name:'',email:'',number:''})
       };
 
     const editData = (index) => {
@@ -57,13 +59,26 @@ const Crudtable = () => {
         updatedData.splice(index, 1);   
         setData(updatedData);           
       };
+
+      const handleChange=(e)=>{
+      const val= e.target.value
+      const idval= e.target.id
+      console.log(e.target.id);
+      setFormInfo((prevFormInfo) => ({
+        ...prevFormInfo,  // Spread the existing state
+        [idval]: val, // Update the specific field
+      }));
+      
+
+      }
+
     return (
         <>
             <form>
-                Name <input type='name' placeholder='enter name' value={name} onChange={(e) => setName(e.target.value)} />
-                Email <input type='email' placeholder='enter mail' value={email} onChange={(e) => setEmail(e.target.value)} />
-                Number <input type='number' placeholder='enter phone no.' value={number} onChange={(e) => setNumber(e.target.value)} />
-                <button onClick={saveData}>click me</button>
+                Name <input type='name' placeholder='enter name'id='name' value={formInfo.name} onChange={(e) => handleChange(e)} /><br/><br/>
+                Email <input type='email' placeholder='enter mail'id='email' value={formInfo.email} onChange={(e) => handleChange(e)} /><br/><br/>
+                Number <input type='number' placeholder='enter phone no.'id='number' value={formInfo.number} onChange={(e) => handleChange(e)} /><br/><br/>
+                <button onClick={saveData}>save</button>
             </form>
             <hr></hr>
 
@@ -77,7 +92,7 @@ const Crudtable = () => {
                     </tr>
                 </thead>
                 <tbody>{
-                    data.map((item, index) => {
+                    data?.map((item, index) => {
 
                         return <tr key={index}>
                             <td>{item.name}</td>
